8f62e408a5a3f019b63d1046629fe2683df70209
oeyoews <jyao4783@gmail.com>
2024-03-28 22:36:13 +0800

    feat: try neotw electron

---
+++ electron/main.ts
@@ -0,0 +1,110 @@
+import {
+  Tray,
+  Menu,
+  nativeImage,
+  BrowserView,
+  app,
+  BrowserWindow,
+} from 'electron';
+import path from 'node:path';
+
+// The built directory structure
+//
+// ├─┬─┬ dist
+// │ │ └── index.html
+// │ │
+// │ ├─┬ dist-electron
+// │ │ ├── main.js
+// │ │ └── preload.js
+// │
+process.env.DIST = path.join(__dirname, '../dist');
+process.env.VITE_PUBLIC = app.isPackaged
+  ? process.env.DIST
+  : path.join(process.env.DIST, '../public');
+
+let win: BrowserWindow;
+// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
+// const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL'];
+const iconpath = path.join(process.env.VITE_PUBLIC, 'vite.png');
+const port = '8000';
+const baseurl = `http://localhost:${port}`;
+let tray = null;
+const icon = nativeImage.createFromPath(iconpath);
+
+function createTiddlyWikiWindow() {
+  win = new BrowserWindow({
+    // show: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    width: 900,
+    height: 600,
+    frame: false,
+    icon,
+    titleBarStyle: 'hidden',
+    titleBarOverlay: {
+      color: '#2f3241',
+      symbolColor: '#74b1be',
+      height: 20,
+    },
+  });
+
+  const view = new BrowserView({});
+  win.setBrowserView(view);
+  const { width, height } = win.getBounds();
+  view.setBounds({ x: 0, y: 0, width, height });
+  view.webContents.on('did-finish-load', () => {
+    // 在网页加载完成后执行插入操作
+    console.log('网页加载完成，可以执行插入操作了。');
+  });
+
+  // view.webContents.on('before-input-event', (event: Event, input: Input) => {});
+
+  view.webContents.loadURL(baseurl); // win.loadurl 无法使用 tiddlywiki 内的快捷键
+
+  // tray
+  tray = new Tray(icon);
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: '关闭',
+      type: 'normal',
+      click: () => win.close(),
+      // icon
+    },
+  ]);
+  tray.setToolTip('neotw');
+  tray.setTitle('neotw');
+  tray.setContextMenu(contextMenu);
+
+  tray.on('click', () => {
+    win.isVisible() ? win.hide() : win.show();
+  });
+
+  // 监听窗口大小变化
+  win.on('resize', () => {
+    const bounds = win.getBounds();
+    view.setBounds({ x: 0, y: 0, width: bounds.width, height: bounds.height });
+  });
+}
+
+// app.whenReady().then(createWindow);
+app.whenReady().then(createTiddlyWikiWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+    // @ts-expect-error
+    win = null;
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createTiddlyWikiWindow();
+  }
+});
